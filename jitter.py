363f53c564a6d8d4270bc9cc74cdbe1321fa0e3b
Laura Verde <100606118+ellevrd@users.noreply.github.com>
2023-08-28 20:01:30 +0200

    Add files via upload

---
+++ jitter.py
@@ -0,0 +1,22 @@
+import parselmouth
+import numpy as np
+
+
+
+def jitter(name):
+    sound = parselmouth.Sound(name)
+    pitch = sound.to_pitch()
+    pulses = parselmouth.praat.call([sound, pitch], "To PointProcess (cc)")
+    voice_report_str = parselmouth.praat.call([sound, pitch, pulses], "Voice report", 0.0, 0.0, 75, 600, 1.3, 1.6, 0.03,
+                                              0.45)
+    jitter_local = parselmouth.praat.call(pulses, "Get jitter (local)", 0.0, 0.0, 0.0001, 0.02, 1.3) * 100
+    return jitter_local
+
+
+if __name__ == '__main__':
+    filename = 'path'
+    ji=jitter(filename)
+    print(ji)
+
+
+
